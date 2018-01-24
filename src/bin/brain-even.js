daab060465ca25a9210c75138c983fa3e08e6954
alexeypant <alexeypant@gmail.com>
2018-01-24 20:59:30 +0300

    all the logic in the index.js now

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,32 +1,8 @@
 #!/usr/bin/env node
 
-import {getResponseAndShow, getRoundRandomNumber, getGuess } from '..';
+import {startGameEven } from '..';
 
 
-console.log('Welcome to the Brain Games');
-
-console.log('Answer "yes" if number even otherwise answer "no".');
-
-const playerName = getResponseAndShow();  // getting the name, saying hello and show the name
+startGameEven();
 
 
-let randomNumber;
-let guess;
-let correctAnswers = 0;
-
-for(let i = 0; i < 3; i++){
-
-	randomNumber = getRoundRandomNumber();
-	console.log(`Question: ${randomNumber}'`);
-	guess = getGuess();
-	if( (randomNumber%2 === 0 && guess === 'yes') || (randomNumber%2 !== 0 && guess === 'no') ) {
-		correctAnswers++;
-		console.log('Correct!');
-		continue;
-	}
-	console.log(`${guess} was wrong answer ;(. Correct answer was  ${(guess === 'yes') ? 'no' : 'yes'}'`);
-	break;
-}
-if (correctAnswers === 3) {
-	console.log(`Congratulations ${playerName}!'`);
-}
--- src/index.js
+++ src/index.js
@@ -3,19 +3,23 @@
 import readlineSync from 'readline-sync';
 
 
-// Wait for user's response
+export const showWelcomeMessageEven = () => {
+	console.log('Welcome to the Brain Games');
+	console.log('Answer "yes" if number even otherwise answer "no".');
+}
 
+
+// Wait for user's response
 export const getResponseAndShow = () => {
-	const userName = readlineSync.question('May I have your name? ') 
+	const userName = readlineSync.question('May I have your name? ');
+
 	console.log(`'Hello, ${userName}!'`);
 	return userName;
-
 }
 
 export const getGuess = () => {
 	const guess = readlineSync.question('Your answer: ');
 	return guess;
-
 }
 
 //export default getResponseAndShow;
@@ -24,4 +28,33 @@ export const getRoundRandomNumber = () => {
 	
 	return Math.round(Math.random()*10);
 
+}
+
+export const startGameEven = () => {
+
+showWelcomeMessageEven();
+
+const playerName = getResponseAndShow();  // getting the name, saying hello and show the name
+
+
+let randomNumber;
+let guess;
+let correctAnswers = 0;
+
+for(let i = 0; i < 3; i++){
+
+	randomNumber = getRoundRandomNumber();
+	console.log(`Question: ${randomNumber}'`);
+	guess = getGuess();
+	if( (randomNumber%2 === 0 && guess === 'yes') || (randomNumber%2 !== 0 && guess === 'no') ) {
+		correctAnswers++;
+		console.log('Correct!');
+		continue;
+	}
+	console.log(`${guess} was wrong answer ;(. Correct answer was  ${(guess === 'yes') ? 'no' : 'yes'}'`);
+	break;
+}
+if (correctAnswers === 3) {
+	console.log(`Congratulations ${playerName}!'`);
+}
 }
