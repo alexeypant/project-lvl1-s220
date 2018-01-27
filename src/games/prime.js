c0ffcaba59761afc797d4b1dbe64063ccf0e9c04
alexeypant <alexeypant@gmail.com>
2018-01-27 13:42:07 +0300

    predicat is corrected

--- src/games/prime.js
+++ src/games/prime.js
@@ -2,20 +2,12 @@ import { cons } from 'hexlet-pairs';
 import { runGame } from '..';
 
 const isPrime = (num) => {
-  for (let i = 2; i < num - 1; i += 1) {
+  for (let i = 2; i < num; i += 1) {
     if (num % i === 0) {
-      return 'no';
+      return false;
     }
-    return 'yes';
   }
-  const start = Math.round(Math.random() * 10);
-  const step = Math.round(Math.random() * 5);
-  const array = [];
-  array[0] = start;
-  for (let i = 1; i < 10; i += 1) {
-    array[i] = array[i - 1] + step;
-  }
-  return array;
+  return true;
 };
 
 const startGame = () => {
@@ -23,13 +15,13 @@ const startGame = () => {
     task: 'Is this number prime?',
     function: () => {
       const number = Math.round(Math.random() * 30) + 2;
-      const correctAnswer = isPrime(number);
+      const correctAnswer = (isPrime(number)) ? 'yes' : 'no';
       const question = (`${number}`);
       return cons(question, correctAnswer);
     },
   };
 
-  const message = 'Welcome to the Brain Games: Progression';
+  const message = 'Welcome to the Brain Games: Prime';
 
   runGame(game, message);
 };
